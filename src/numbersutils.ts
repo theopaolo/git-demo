51d11cd93a66a3716042d9bb153e19ca77b50b43
theopaolo <tpgoedert@outlook.com>
2025-10-28 12:02:04 +0100

    fix tva calculator

--- src/numbersutils.ts
+++ src/numbersutils.ts
@@ -1,4 +1,4 @@
 function totalWithVAT(amount: number, vat: number) {
   if (amount < 0) throw new Error("amount<0");
-  return Math.round(amount * (1 + vat) * 100) / 100;
+  return Math.round(amount * (1 + vat) * 10) / 100;
 }
